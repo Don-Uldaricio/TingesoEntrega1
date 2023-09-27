b460e261b5dd62405f1fb600f8aed80ae68ae737
Nicolás Aguilera <nicolas.aguilera.g@usach.cl>
2023-09-27 17:56:46 -0300

    feat: add maven project to path

---
+++ entrega1/src/test/java/com/tingeso/entrega1/Entrega1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tingeso.entrega1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Entrega1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
